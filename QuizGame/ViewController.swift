b574808af76e1ff740046f5b806a163f099ee02a
Kayla Davis <kayla.davis.17@cnu.edu>
2021-03-09 15:57:29 -0500

    Initial Commit

---
+++ QuizGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizGame
+//
+//  Created by Kayla Davis on 3/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

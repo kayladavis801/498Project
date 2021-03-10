f35e2d507b6ce65e67f9e8096e751f89efb027a1
Kayla Davis <kayla.davis.17@cnu.edu>
2021-03-09 19:01:37 -0500

    first commit

--- QuizGame/ViewController.swift
+++ QuizGame/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Kayla Davis on 3/9/21.
 //
 
+
 import UIKit
 
 class ViewController: UIViewController {
@@ -12,6 +13,13 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    //starting the game
+    @IBAction func startGame(){
+        let vc = storyboard?.instantiateViewController(identifier: "game") as! GameViewController
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated:true)
     }
 
 
